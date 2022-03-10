c8b2b6eb0be5047a7b76a5572c85d96af5bc47a1
gitevanlu <cenclevan@yahoo.com>
2022-03-10 14:15:57 +0800

    c update

--- Cpp-Crash/test-1.cpp
+++ Cpp-Crash/test-1.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
+#include <iostream> // header file-"Library" 
+                    // input output stream
 
-int main() {
+int main()
+{ // main: main function
   std::cout << "Hello World!\n"; // this will output the console hello world.
-}
+  // cout : consloe output
+  // cin : console input
+  // std : name of the base for using cout and cin
+  system("pause>0");
+} 
