161ab420e7bca40db4ca143e7211356f91776f88
gitevanlu <cenclevan@yahoo.com>
2022-03-12 15:02:41 +0800

    update switch

--- Cpp-Crash/chapter-3-Calculator-APP-Swich.cpp
+++ Cpp-Crash/chapter-3-Calculator-APP-Swich.cpp
@@ -5,13 +5,36 @@ int main() {
   float num1, num2;  // declare two float variables
   char operatorCalc; // declare the operatoras  a char variable which we will
                      // use to store the operator
-  bool isNum1Int, isNum2Int; // declare two bool variables which we will store
-                             // num1 and num2 if they are integers.
+  bool isNum1Int, isNum2Int; // declare two bool variables to be determine
+                             // whether num1 and num2 are integers.
   cout << "Please enter the number and operator: " << endl;
 
   cin >> num1 >> operatorCalc >>
-      num2; // declare the function we will use in the calculator.
+      num2; // declare the expression we will use in the calculator.
 
-  //   cout << "Please enter the number and operator: " << endl;
-  //   cin >> num1 >> operatorCalc >> num2;
+  switch (operatorCalc) {
+  case '-':
+    cout << num1 << operatorCalc << num2 << " = " << num1 - num2 << endl;
+    break;
+  case '+':
+    cout << num1 << operatorCalc << num2 << " = " << num1 + num2 << endl;
+    break;
+  case '*':
+    cout << num1 << operatorCalc << num2 << " = " << num1 * num2 << endl;
+    break;
+  case '/':
+    cout << num1 << operatorCalc << num2 << " = " << num1 / num2 << endl;
+    break;
+  case '%':
+    isNum1Int = (int(num1) == num1);
+    isNum2Int = (int(num2) == num2);
+    if (isNum1Int && isNum2Int) // if they are both true.
+      cout << num1 << operatorCalc << num2 << " = " << int(num1) % int(num2)
+           << endl;
+    else
+      cout << "Invalid input" << endl;
+    break;
+  default:
+    cout << "Invalid calculation" << endl;
+  }
 }
--- Cpp-Crash/cpp-notes.md
+++ Cpp-Crash/cpp-notes.md
@@ -673,9 +673,53 @@ switch (expression)  {
 
 Where the expression is corresponding to constantX.
 
+With this method, let's code the calculator with switch case statement.
+
 ```cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+  float num1, num2;  // declare two float variables
+  char operatorCalc; // declare the operatoras  a char variable which we will
+                     // use to store the operator
+  bool isNum1Int, isNum2Int; // declare two bool variables to be determine
+                             // whether num1 and num2 are integers.
+  cout << "Please enter the number and operator: " << endl;
+
+  cin >> num1 >> operatorCalc >>
+      num2; // declare the expression we will use in the calculator.
+
+  switch (operatorCalc) {
+  case '-':
+    cout << num1 << operatorCalc << num2 << " = " << num1 - num2 << endl;
+    break;
+  case '+':
+    cout << num1 << operatorCalc << num2 << " = " << num1 + num2 << endl;
+    break;
+  case '*':
+    cout << num1 << operatorCalc << num2 << " = " << num1 * num2 << endl;
+    break;
+  case '/':
+    cout << num1 << operatorCalc << num2 << " = " << num1 / num2 << endl;
+    break;
+  case '%':
+    isNum1Int = (int(num1) == num1);
+    isNum2Int = (int(num2) == num2);
+    if (isNum1Int && isNum2Int) // if they are both true.
+      cout << num1 << operatorCalc << num2 << " = " << int(num1) % int(num2)
+           << endl;
+    else
+      cout << "Invalid input" << endl;
+    break;
+  default:
+    cout << "Invalid calculation" << endl;
+  }
+}
 
 ```
+
+yeah, we made the defalut as the
 
 ```cpp
 
