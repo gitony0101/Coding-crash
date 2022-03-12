b9405140781a2d65e18896ab3b99f8ba1aadc573
gitevanlu <cenclevan@yahoo.com>
2022-03-12 08:21:32 +0800

    update

--- Cpp-Crash/chapter-3-OperatorsEX.cpp
+++ Cpp-Crash/chapter-3-OperatorsEX.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {}
+int main() {
+  int a = 5, b = 4;
+  cout << a + (b++) << endl; // here b still = 4 because the expression is
+                             // evaluated before the increment
+  cout << b << endl; // here b = 5 because the expression is evaluated after
+                     // the increment
+  cout << (--a) + b << endl; // Now a = 4 since pre-decrement is evaluated
+                             // before the expression, also, b is 5 now
+  cout << !(true && false) << endl;
+  cout << !(true || false) << endl;
+}
