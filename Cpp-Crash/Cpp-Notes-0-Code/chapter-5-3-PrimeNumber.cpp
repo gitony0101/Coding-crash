8150e9f0e8d4676bfa8b8050af72322ce0241921
gitevanlu <cenclevan@yahoo.com>
2022-03-16 18:47:04 +0800

    update

--- Cpp-Crash/Cpp-Notes-0-Code/chapter-5-3-PrimeNumber.cpp
+++ Cpp-Crash/Cpp-Notes-0-Code/chapter-5-3-PrimeNumber.cpp
@@ -6,11 +6,17 @@ int main() {
   cout << " Please enter a number : ";
   cin >> number;
 
-  // ket loop here:
-  bool isPrimeFlag = true; // Presumption of prime number(true)
+  // key loop here:
+  // bool isPrimeFlag = true; // Presumption of prime number(true)
 
   for (int i = 2; i < number; i++) {
     if (number % i == 0) {
+      cout << "The number is NOT a prime number" << endl;
+      break;
+
+    } else {
+      cout << "The number is a prime number" << endl;
+      break;
     }
   }
 }
