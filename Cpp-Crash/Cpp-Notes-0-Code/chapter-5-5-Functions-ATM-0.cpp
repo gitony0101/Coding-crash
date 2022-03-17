b742576d6bca74c9d44a4405b8745eb5eb5f697f
gitevanlu <cenclevan@yahoo.com>
2022-03-17 12:45:21 +0800

    atm

--- Cpp-Crash/Cpp-Notes-0-Code/chapter-5-5-Functions-ATM-0.cpp
+++ Cpp-Crash/Cpp-Notes-0-Code/chapter-5-5-Functions-ATM-0.cpp
@@ -10,43 +10,40 @@ void showMenu() {
   cout << "*******************************" << endl;
 }
 
-int option;
-double balance = 500;
+int main() {
+  int option;
+  double balance = 500;
 
-do {
-}
-{}
+  // Check balance,deposit,withdraw,show menu.
 
-// Check balance,deposit,withdraw,show menu.
+  showMenu();
+  cout << "Press the number of the option: ";
+  cin >> option;
+  system("cls");
 
-showMenu();
-cout << "Press the number of the option: ";
-cin >> option;
-system("cls");
-
-switch (option) {
-case 1:
-  cout << "Balance:" << balance << "$" << endl;
-case 2:
-  cout << "Deposit amount:";
-  double depositamount;
-  cin >> depositamount;
-  balance += depositamount;
-  break;
-case 3:
-  cout << "Please enter the money you want to withdraw:";
-  double withdrawamount;
-  cin >> withdrawamount;
-
-  if (withdrawamount <= balance) {
-    balance -= withdrawamount;
+  switch (option) {
+  case 1:
+    cout << "Balance:" << balance << "$" << endl;
+  case 2:
+    cout << "Deposit amount:";
+    double depositamount;
+    cin >> depositamount;
+    balance += depositamount;
     break;
-  } else {
-    cout << "You do not have enough balance." << endl;
+  case 3:
+    cout << "Please enter the money you want to withdraw:";
+    double withdrawamount;
+    cin >> withdrawamount;
+
+    if (withdrawamount <= balance) {
+      balance -= withdrawamount;
+      break;
+    } else {
+      cout << "You do not have enough balance." << endl;
+      break;
+    }
+  case 4:
+    cout << "Thank you, see you next time!" << endl;
+    break;
   }
-}
-while (option = 4) {
-  cout << "Thank you, see you next time!" << endl;
-  break;
-}
 }
