5b7016ce7be7491f534dae564fa1f8dfac7ae942
gitevanlu <cenclevan@yahoo.com>
2022-03-23 18:10:10 +0800

    update

--- Cpp-Crash/Cpp-Code-Crash-STL/STL-1-selfDefinedIterator.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/STL-1-selfDefinedIterator.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -27,11 +28,32 @@ void test_03() {
 
   // Every container has the iterator of its own.
   // like vector<Person>::iterator
+  //
   for (vector<Person>::iterator it = v.begin(); it != v.end(); it++) {
-    cout << it->m_name << " " << it->m_age << endl;
+    // cout << it->m_name << " " << it->m_age << endl;
+    printf("Name : %s\n, Age: %d\n.", it->m_name.c_str(), it->m_age);
+  }
+
+  for (vector<Person>::iterator it = v.begin(); it != v.end(); it++) {
+    cout << "Name: " << (*it).m_name << "Age : " << (*it).m_age << endl;
   }
 }
 
+// Store self defiend datatype POINTER
+void test_04() {
+  vector<Person> v;
+  Person p1("Tony", 8);
+  Person p2("Tom", 9);
+  Person p3("Jack", 10);
+  Person p4("Jill", 11);
+
+  v.push_back(p1);
+  v.push_back(p2);
+  v.push_back(p3);
+  v.push_back(p4);
+}
+
+// RUN
 int main() {
   test_03();
   return 0;
