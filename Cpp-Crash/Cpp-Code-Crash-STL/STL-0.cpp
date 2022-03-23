05466db667479dd5121fb5a44c5d4684bbcabc24
gitevanlu <cenclevan@yahoo.com>
2022-03-23 12:23:37 +0800

    update

--- Cpp-Crash/Cpp-Code-Crash-STL/STL-0.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/STL-0.cpp
@@ -2,16 +2,17 @@
 #include <iostream>
 #include <vector> // vector library
 using namespace std;
+
 // native Pointers are iterators,too.
-void test_01() {
-  int arr[5] = {1, 2, 3, 4, 5};
-  int *p = arr;
-  for (int i = 0; i < 5; i++) {
-    // printf("Value of arr :%d\n", arr[i]);
-    // printf("s &d\n", *(p[i]));
-    cout << *(p++) << endl;
-  }
-}
+// void test_01() {
+//   int arr[5] = {1, 2, 3, 4, 5};
+//   int *p = arr;
+//   for (int i = 0; i < 5; i++) {
+//     // printf("Value of arr :%d\n", arr[i]);
+//     // printf("s &d\n", *(p[i]));
+//     cout << *(p++) << endl;
+//   }
+// }
 
 void test_02() {
   vector<int>
@@ -22,9 +23,16 @@ void test_02() {
   v.push_back(40);
   // traverse elements
   vector<int>::iterator itBegin =
-      v.begin(); // v.begin() Initial iterator, points to the firs element of
+      v.begin(); // v.begin() In®itial iterator, points to the firs element of
                  // the container.
   vector<int>::iterator itEnd = v.end(); // v.end() end iterator,points to the
                                          // next poistion of the last element.
+
+  // 1st traverse
+  while (itBegin != itEnd) {
+    cout << *itBegin << endl;
+    itBegin++;
+  }
 }
+// 2nd traverse
 // int main() {}
---
+++ Cpp-Crash/Cpp-Code-Crash-STL/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+void test_01() {
+//   int arr[5] = {1, 2, 3, 4, 5};
+//   int *p = arr;
+//   for (int i = 0; i < 5; i++) {
+//     // printf("Value of arr :%d\n", arr[i]);
+//     // printf("s &d\n", *(p[i]));
+//     cout << *(p++) << endl;
+//   }
+// }
