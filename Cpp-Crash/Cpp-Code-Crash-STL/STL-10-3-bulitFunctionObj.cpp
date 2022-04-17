e4c96931ddfa5b0cd8f63231b9dcb2fcb3fdf0d2
gitevanlu <cenclevan@yahoo.com>
2022-04-17 17:26:55 +0800

    up

--- Cpp-Crash/Cpp-Code-Crash-STL/STL-10-3-bulitFunctionObj.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/STL-10-3-bulitFunctionObj.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <functional> // 第一步导入 内建函数对象 头文件
 #include <iostream>
 using namespace std;
@@ -9,8 +10,30 @@ using namespace std;
 
 这些仿函数产生的对象、用法和一般函数完全相同
 内建函数对象需要引入头文件 #include <functional>
+加减乘除  取反
+逻辑与或非
+
 */
 
-void myFunc() {}
+void testBuilinFunc() {
+  //算术仿函数
+  // negate
+  negate<int> n;
+  printf("negate 50 : %d\n", n(50));
+  // plus
+  plus<int> p;
+  printf("10 + 20= %d\n", p(10, 20));
+}
 
-int main() {}
+int main() { testBuilinFunc(); }
+
+/*
+仿函数原型
+
+* `template<class T> T plus<T>`                //加法仿函数
+* `template<class T> T minus<T>`              //减法仿函数
+* `template<class T> T multiplies<T>`    //乘法仿函数
+* `template<class T> T divides<T>`         //除法仿函数
+* `template<class T> T modulus<T>`         //取模仿函数
+* `template<class T> T negate<T>`           //取反仿函数
+*/
