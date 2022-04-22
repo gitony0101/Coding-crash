65ef533deef14d3ee757c9f70f519069149c9d75
gitevanlu <cenclevan@yahoo.com>
2022-04-22 20:07:47 +0800

    sorting
    Signed-off-by: gitevanlu <cenclevan@yahoo.com>

--- Cpp-Crash/Cpp-Code-Crash-STL/STL-Alg- Sorting.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/STL-Alg- Sorting.cpp
@@ -15,10 +15,25 @@ using namespace std;
 
 // sort最常用
 // sort(beg,end,predicate)
+class MyPrint { // 定义打印int值仿函数
+public:
+  void operator()(int val) { printf("%d\n", val); }
+};
 
-void exSort() {}
+void exSort() {
+  vector<int> v;
+  v.push_back(10);
+  v.push_back(30);
+  v.push_back(50);
+  v.push_back(20);
+  v.push_back(40);
+  // sort 从小到大排序
+  sort(v.begin(), v.end());                // 默认排序
+  for_each(v.begin(), v.end(), MyPrint()); //遍历vector，使用MyPrint()打印
+  cout << endl;
+}
 
 // merge 容器内元素合并 并存储到另外一个容器中
 // 注意：合并容器内序列必须有序，新容器内必须有容量
 
-int main() {}
+int main() { exSort(); }
