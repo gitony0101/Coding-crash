03921f2f2dbb8d5e5bd7aa5e34949f7626e0928f
gitevanlu <cenclevan@yahoo.com>
2022-04-22 20:50:58 +0800

    打印仿函数 全局调用
    Signed-off-by: gitevanlu <cenclevan@yahoo.com>

--- Cpp-Crash/Cpp-Code-Crash-STL/STL-Alg-Sorting.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/STL-Alg-Sorting.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cstdio>
+#include <functional>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,17 +9,18 @@ using namespace std;
 /*
 常用排序算法
 1. merge 合并，将两个有序序列的容器合并到目标容器中，目标容器必须有容量。
-2. sort 排序 最常用
-3. random_shuffle 洗牌，打乱顺序
+1. sort 排序 最常用
+2. random_shuffle 洗牌，打乱顺序
 4. reverse 逆序
 */
 
-// sort最常用
-// sort(beg,end,predicate)
-class MyPrint { // 定义打印int值仿函数
+class myPrint { //打印仿函数 全局调用
 public:
   void operator()(int val) { printf("%d\n", val); }
 };
+
+// sort最常用
+// sort(beg,end,predicate)
 
 void exSort() {
   vector<int> v;
@@ -29,11 +31,15 @@ void exSort() {
   v.push_back(40);
   // sort 从小到大排序
   sort(v.begin(), v.end());                // 默认排序
-  for_each(v.begin(), v.end(), MyPrint()); //遍历vector，使用MyPrint()打印
+  for_each(v.begin(), v.end(), myPrint()); //遍历vector，使用MyPrint()打印
   cout << endl;
+  //从大到小 使用内置函数greater
+  sort(v.begin(), v.end(), greater<int>()); // 使用内置函数greater< >
+  for_each(v.begin(), v.end(), myPrint()); //遍历vector，使用MyPrint()打印
 }
 
 // merge 容器内元素合并 并存储到另外一个容器中
 // 注意：合并容器内序列必须有序，新容器内必须有容量
+// random shuffle (beg end)洗牌   指定范围内的元素随机调整次序
 
 int main() { exSort(); }
