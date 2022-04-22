97f610976bb059ba97667924d17729b14af9f9a0
gitevanlu <cenclevan@yahoo.com>
2022-04-22 14:33:52 +0800

    sort
    Signed-off-by: gitevanlu <cenclevan@yahoo.com>

---
+++ Cpp-Crash/Cpp-Code-Crash-STL/STL-Alg- Sorting.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+常用排序算法
+1. merge 合并，将两个有序序列的容器合并到目标容器中，目标容器必须有容量。
+2. sort 排序 最常用
+3. random_shuffle 洗牌，打乱顺序
+4. reverse 逆序
+*/
+
+// sort最常用
+// sort(beg,end,predicate)
+
+void exSort() {}
+
+// merge 容器内元素合并 并存储到另外一个容器中
+// 注意：合并容器内序列必须有序，新容器内必须有容量
+
+int main() {}
