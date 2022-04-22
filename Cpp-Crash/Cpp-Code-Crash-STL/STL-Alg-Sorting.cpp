5c9098ba415bd81ac6d364ba2eceb612dbecf196
gitevanlu <cenclevan@yahoo.com>
2022-04-22 23:11:53 +0800

    shuffle
    Signed-off-by: gitevanlu <cenclevan@yahoo.com>

--- Cpp-Crash/Cpp-Code-Crash-STL/STL-Alg-Sorting.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/STL-Alg-Sorting.cpp
@@ -1,16 +1,21 @@
 #include <algorithm>
 #include <cstdio>
+#include <cstdlib>
+#include <experimental/algorithm> // random_shuffle 已经删除，这个用来调用shuffle
 #include <functional>
 #include <iostream>
+#include <random> // 设定种子用
 #include <string>
 #include <vector>
 using namespace std;
 
 /*
 常用排序算法
-1. merge 合并，将两个有序序列的容器合并到目标容器中，目标容器必须有容量。
+
 1. sort 排序 最常用
-2. random_shuffle 洗牌，打乱顺序
+2. shuffle 洗牌，打乱顺序 random_shuffle 已经不用了
+
+3. merge 合并，将两个有序序列的容器合并到目标容器中，目标容器必须有容量。
 4. reverse 逆序
 */
 
@@ -41,5 +46,25 @@ void exSort() {
 // merge 容器内元素合并 并存储到另外一个容器中
 // 注意：合并容器内序列必须有序，新容器内必须有容量
 // random shuffle (beg end)洗牌   指定范围内的元素随机调整次序
+void exShuffle() {
+  vector<int> v;
+  for (int i = 0; i < 10; i++) {
+    v.push_back(i);
+  }
 
-int main() { exSort(); }
+  printf("Original order: ");
+  for_each(v.begin(), v.end(), myPrint());
+  printf("After shuffle:\n");
+  random_device rnd;      // 设定随机数
+  mt19937_64 seed(rnd()); // 设定随机数
+
+  shuffle(v.begin(), v.end(), seed);
+  for_each(v.begin(), v.end(), myPrint());
+}
+
+// merge 合并，将两个有序序列的容器合并到目标容器中，目标容器必须有容量
+
+int main() {
+  // exSort();
+  exShuffle(); // error: use of undeclared identifier 'random_shuffle'
+}
