fc1a046c3319d634f6278c62029d452d4bd3bee1
gitevanlu <cenclevan@yahoo.com>
2022-04-26 07:32:13 +0800

    update
    Signed-off-by: gitevanlu <cenclevan@yahoo.com>

--- Cpp-Crash/Cpp-Code-Crash-STL/STL-Alg-Copy-Replace.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/STL-Alg-Copy-Replace.cpp
@@ -9,9 +9,9 @@ copy(iterator beg, iterator end, iterator dest); //
 容器内指定范围的元素拷贝到另一容器中，目标容器记得提前开辟空间
 replace(iterator beg, iterator end, oldvalue, newvalue); //
 将容器内指定范围的所有旧元素修改为新元素
-replace_if(iterator beg, iterator end, _pred, newvalue)//
-容器内指定范围满足条件的元素替换为新元素，注意有谓词，仿函数上线
-- `swap`                     // 互换两个容器的元素
+replace_if(iterator beg, iterator end, _pred, newvalue);//
+容器内指定范围满足条件的元素替换为新元素，可以利用仿函数灵活筛选满足的条件
+swap(container c1, container c2);// 互换两个容器的元素
 */
 class myPrint {
 public:
@@ -49,13 +49,45 @@ class ReplaceGreater30 { //创造replace_if 里面的使用的仿函数
 public:
   bool operator()(int val) { return val >= 30; }
 };
-class vecV1 { //试着来一个公共的vector容器全局可用
+
+class vecV1 { // samstrongman
 public:
-  vector<int> v0;
+  vecV1(vector<int> &v1) {
+    v1.push_back(20);
+    v1.push_back(30);
+    v1.push_back(20);
+    v1.push_back(40);
+    v1.push_back(50);
+    v1.push_back(10);
+    v1.push_back(20);
+  }
 };
-void exReplace_if() {}
+
+void test01() { // samstrongman
+  vector<int> vecTest;
+  vecV1 helloworld(vecTest); // helloworld is an object name
+  for_each(vecTest.begin(), vecTest.end(), myPrint());
+}
+
+void exReplace_if() {
+  vector<int> v;
+  v.push_back(20);
+  v.push_back(30);
+  v.push_back(20);
+  v.push_back(40);
+  v.push_back(50);
+  v.push_back(10);
+  v.push_back(20);
+  printf("替换前：\n");
+  for_each(v.begin(), v.end(), myPrint());
+  printf("替换后；\n");
+  replace_if(v.begin(), v.end(), ReplaceGreater30(), 30000);
+  for_each(v.begin(), v.end(), myPrint());
+}
 
 int main() {
   //   exCopy();
-  exReplace();
+  //   exReplace();
+  test01(); // samstrongman
+  //   exReplace_if();
 }
