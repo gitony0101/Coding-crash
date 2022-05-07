ac61392640d6a204dc5c85a885d48770266a00b9
gitevanlu <cenclevan@yahoo.com>
2022-05-07 20:12:12 +0800

    if else switch case
    Signed-off-by: gitevanlu <cenclevan@yahoo.com>

--- Cpp-Crash/Cpp-Code-Crash-STL/chapter-2-3-operatorCalc.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/chapter-2-3-operatorCalc.cpp
@@ -20,4 +20,35 @@ void exOperatorCalc01() { // if-else version
   ;
 }
 
-int main() { exOperatorCalc01(); }
+void exOperatorCalc02() { // switch case
+  float num1, num2;
+  char operatorCalc;
+  // 创建一个新的 bool 判别是否输入为整数
+  bool isNum1Int, isNum2Int;
+  printf("Please enter the calculation(number and the calculator)\n");
+  cin >> num1 >> operatorCalc >> num2;
+  switch (operatorCalc) {
+  case '+':
+    printf("%f + %f = %f\n", num1, num2, num1 + num2);
+  case '-':
+    printf("%f - %f = %f\n", num1, num2, num1 - num2);
+  case '*':
+    printf("%f * %f = %f\n", num1, num2, num1 * num2);
+  case '/':
+    printf("%f / %f = %f\n", num1, num2, num1 / num2);
+    break;
+  case '%':
+    isNum1Int = (int(num1) == num1);
+    isNum2Int = (int(num2) == num2);
+    if (isNum1Int && isNum2Int)
+      printf("%d moldulate %d = %d\n", int(num1), int(num2),
+             int(num1) % int(num2));
+    break;
+  default:
+    printf("Invalid input.\n");
+  }
+}
+int main() {
+  // exOperatorCalc01();
+  exOperatorCalc02();
+}
