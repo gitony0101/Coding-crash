510aa78685fdd72f4c27ef26eeaf77f9d5fd531b
gitevanlu <evanlu08@hotmail.com>
2022-05-13 16:43:51 +0800

    update
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- Cpp-Crash/Cpp-Code-Crash-STL/chapter-4-1-exFunction.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/chapter-4-1-exFunction.cpp
@@ -1,4 +1,6 @@
+#include <cstdio>
 #include <iostream>
+#include <string>
 using namespace std;
 
 /*
@@ -6,6 +8,18 @@ using namespace std;
 
 */
 
-void introducing(string name, int age, string city = 'NJ') {}
+void introducing(string name, int age, string city = "NJ") {
+  printf("Hello, My name is %s,I'm %d years old, I'm from %s.\n", name.c_str(),
+         age, city.c_str());
+}
 
-int main() {}
+void printIntro() {
+  string name_Input, city_Input;
+  int age_input;
+  cout << "Please enter your name: " << endl;
+  cin >> name_Input;
+  cout << "Please enter your age: " << endl;
+  cin >> age_input;
+  introducing(name_Input, age_input);
+}
+int main() { printIntro(); }
