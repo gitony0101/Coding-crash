bc1421d622f699bd7badd546b76d034d83bf8885
gitevanlu <evanlu08@hotmail.com>
2022-05-13 23:03:35 +0800

    update
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- Cpp-Crash/Cpp-Code-Crash-STL/chapter-4-1-exFunction.cpp
+++ Cpp-Crash/Cpp-Code-Crash-STL/chapter-4-1-exFunction.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 /*
 不要直接用 "function" 作为函数名字，真土
-
+自我介绍，参数传递
 */
-
+// 自我介绍，两个函数
 void introducing(string name, int age, string city = "NJ") {
   printf("Hello, My name is %s,I'm %d years old, I'm from %s.\n", name.c_str(),
          age, city.c_str());
@@ -16,10 +16,26 @@ void introducing(string name, int age, string city = "NJ") {
 void printIntro() {
   string name_Input, city_Input;
   int age_input;
-  cout << "Please enter your name: " << endl;
+  printf("Please enter your name:\n");
   cin >> name_Input;
-  cout << "Please enter your age: " << endl;
+  printf("Please enter your age: \n");
   cin >> age_input;
   introducing(name_Input, age_input);
 }
-int main() { printIntro(); }
+// 检验质数
+bool isPrimeNumber(int number) {
+  bool isPrimeFlag = true;
+  for (int i = 2; i < number; i++) {
+    if (number % i == 0) {
+      isPrimeFlag = false;
+      break;
+    }
+  }
+  return isPrimeFlag;
+}
+
+int main() {
+  introducing("Tony", 8);
+
+  printIntro();
+}
