ac09f01bd0bb6f04eaf17c1106b96fa3cd75f8b5
gitevanlu <evanlu08@hotmail.com>
2022-05-15 19:42:51 +0800

    update

--- Cpp-CS225UIUC/L2-Classes.cpp
+++ Cpp-CS225UIUC/L2-Classes.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 void exClasses01() {
-  int myFavoriteInt;
+  int myFavoriteInt = 4;
+  string ss = "ss";
   char grade = 'A';
   double gamma = 0.653;
+  printf("The location in memory of grade is %p\n", &grade);
 }
 
-int main() {}
+int main() { exClasses01(); }
