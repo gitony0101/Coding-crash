b045349c59b495aa1068c32b0c50fad783ff1d94
gitevanlu <evanlu08@hotmail.com>
2022-05-22 16:48:42 +0800

    pointers
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

---
+++ CS-225-UIUC/codes/L3-pointers.cpp
@@ -0,0 +1,5 @@
+#include "L2-Cube.h"
+#include <iostream>
+using namespace std;
+
+int main() {}
--- Cpp-Crash/codes/chapter-5-4-Functions-Overloading.cpp
+++ Cpp-Crash/codes/chapter-5-4-Functions-Overloading.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 // Function Overloading
-int sum(int a, int b);
-double sum(double a, double b);
-float sum(float a, float b, float c);
 
 int main() {
+  int sum(int a, int b);
+  double sum(double a, double b);
+  float sum(float a, float b, float c);
   cout << "Sum of two integers: " << sum(10, 20) << endl;
   cout << "Sum of two doubles: " << sum(10.5, 20.5) << endl;
   cout << "Sum of three floats: " << sum(10.5, 20.5, 30.5) << endl;
 }
 
-int sum(int a, int b) { return a + b; }
-double sum(double a, double b) { return a + b; }
-float sum(float a, float b, float c) { return a + b + c; }
+// int sum(int a, int b) { return a + b; }
+// double sum(double a, double b) { return a + b; }
+// float sum(float a, float b, float c) { return a + b + c; }
