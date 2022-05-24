eb4babe99afdf23587e7140877461b126e9a96b0
gitevanlu <evanlu08@hotmail.com>
2022-05-24 19:52:05 +0800

    全局局部静态
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- Cpp-Crash/codes/1-3-GlobalStaticConst.cpp
+++ Cpp-Crash/codes/1-3-GlobalStaticConst.cpp
@@ -1,21 +1,7 @@
 #include <cstdio>
 #include <iostream>
-//
-#include <stdio.h>
-#define VariableName(name) #name
-//
-#include <typeinfo>
-#define getName(var) #var
-using namespace std;
 
-// 仿函数 实现实现打印功能
-class myPrint {
-public:
-  void operator()(int val) {
-    printf("%s 的 原始地址为 %p,十进制内存地址为%d\n", getName(val), &val,
-           (int)(size_t)&val);
-  }
-};
+using namespace std;
 
 /*全局区
 全局变量 静态变量 常量
@@ -24,9 +10,14 @@ public:
 //全局变量
 int g_a = 10;
 int g_b = 10;
-//全局常量
+int g_c;
+// const修饰的全局变量，即全局常量
 const int c_g_a = 10;
 const int c_g_b = 10;
+// static 静态全局变量
+static int s_g_a = 10;
+static int s_g_b = 10;
+static int s_g_c;
 
 int main() {
   //进入main函数以后，就是局部变量、常量以及静态变量
@@ -34,11 +25,57 @@ int main() {
   int a = 10;
   int b = 10;
   //打印 全局和局部变量地址：
-  myPrint()(g_a);
-  myPrint()(g_b);
+  printf("注意：所有数值变量取值都是10\n全局变量地址打印：\n");
+  /// 真难找，注意这里的表示方法:(int)(size_t)&g_a)
+  printf("全局变量g_a 原始地址为 %p，十进制地址%d\n", &g_a, (int)(size_t)&g_a);
+  printf("全局变量g_b 原始地址为 %p，十进制地址%d\n", &g_b, (int)(size_t)&g_b);
+  printf("局部变量地址打印：\n");
+  printf("局部变量a 原始地址为 %p，十进制地址%d\n", &a, (int)(size_t)&a);
+  printf("局部变量b 原始地址为 %p，十进制地址%d\n", &b, (int)(size_t)&b);
+  //打印静态变量
+  static int s_a = 10;
+  static int s_b = 10;
+  printf("静态变量地址打印：\n");
+  printf("静态变量s_a 原始地址为 %p，十进制地址%d\n", &s_a, (int)(size_t)&s_a);
+  printf("静态变量s_b 原始地址为 %p，十进制地址%d\n", &s_b, (int)(size_t)&s_b);
+  //打印全局常量
+  printf("全局常量地址打印：\n");
+  printf("全局常量c_g_a 原始地址为 %p，十进制地址%d\n", &c_g_a,
+         (int)(size_t)&c_g_a);
+  printf("全局常量c_g_b 原始地址为 %p，十进制地址%d\n", &c_g_b,
+         (int)(size_t)&c_g_b);
+  // 打印局部常量
+  const int c_l_a = 10;
+  const int c_l_b = 10;
+  printf("局部常量地址打印：\n");
+  printf("局部常量c_l_a 原始地址为 %p，十进制地址%d\n", &c_l_a,
+         (int)(size_t)&c_l_a);
+  printf("局部常量c_l_b 原始地址为 %p，十进制地址%d\n", &c_l_b,
+         (int)(size_t)&c_l_b);
+  //打印静态全局变量
+  printf("静态全局变量地址打印：\n");
+  printf("静态全局变量s_g_a 原始地址为 %p，十进制地址%d\n", &s_g_a,
+         (int)(size_t)&s_g_a);
+  printf("静态全局变量s_g_b 原始地址为 %p，十进制地址%d\n", &s_g_b,
+         (int)(size_t)&s_g_b);
 }
 
-// printf("全局变量g_a 原始地址为 %p，十进制地址%d\n", &g_a,
-//        (int)(size_t)&g_a); // 真难找，注意这里的表示方法
-// printf("全局变量g_b 原始地址为 %p，十进制地址%d\n", &g_b, (int)(size_t)&g_b);
+/*以下是之前的尝试：
+尝试使用仿函数打印这些地址，但是在提取变量名称的时候卡住了
+怎么搞？
+#include <stdio.h>
+#define VariableName(name) #name
+//
+#include <typeinfo>
+#define getName(var) #var
+
+// 仿函数 实现实现打印功能
+class myPrint {
+public:
+  void operator()(int val) {
+    printf("%d 的 原始地址为 %p,十进制内存地址为%d\n", val, &val,
+           (int)(size_t)&val);
+  }
+};
+*/
 // myPrint()(a);
