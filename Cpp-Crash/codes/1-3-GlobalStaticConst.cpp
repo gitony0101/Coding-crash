d7f4d9bb0cd29211e55a7c3619a9980aeb690011
gitevanlu <cenclevan@yahoo.com>
2022-05-24 15:33:33 +0800

    printf("全局变量g_a 原始地址为 %p，十进制地址%d\n", &g_a,          (int)(size_t)&g_a)
    Signed-off-by: gitevanlu <cenclevan@yahoo.com>

--- Cpp-Crash/codes/1-3-GlobalStaticConst.cpp
+++ Cpp-Crash/codes/1-3-GlobalStaticConst.cpp
@@ -1,9 +1,24 @@
+#include <cstdio>
 #include <iostream>
 using namespace std;
 
 /*全局区
 全局变量 静态变量 常量
-
 */
 
-int main() {}
+//全局变量
+int g_a = 10;
+int g_b = 10;
+//全局常量
+const int c_g_a = 10;
+const int c_g_b = 10;
+
+int main() {
+  //进入main函数以后，就是局部变量、常量以及静态变量
+  //局部变量
+  int a = 10;
+  int b = 10;
+  //打印 全局和局部变量地址：
+  printf("全局变量g_a 原始地址为 %p，十进制地址%d\n", &g_a,
+         (int)(size_t)&g_a); // 真难找，注意这里的表示方法
+}
