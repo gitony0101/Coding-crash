7bd84ba1610416795116c68bca7922f8186ab264
gitevanlu <cenclevan@yahoo.com>
2022-05-24 16:56:01 +0800

    how to print variable names in C++ printf??
    Signed-off-by: gitevanlu <cenclevan@yahoo.com>

--- Cpp-Crash/codes/1-3-GlobalStaticConst.cpp
+++ Cpp-Crash/codes/1-3-GlobalStaticConst.cpp
@@ -1,6 +1,21 @@
 #include <cstdio>
 #include <iostream>
+//
+#include <stdio.h>
+#define VariableName(name) #name
+//
+#include <typeinfo>
+#define getName(var) #var
 using namespace std;
+
+// 仿函数 实现实现打印功能
+class myPrint {
+public:
+  void operator()(int val) {
+    printf("%s 的 原始地址为 %p,十进制内存地址为%d\n", getName(val), &val,
+           (int)(size_t)&val);
+  }
+};
 
 /*全局区
 全局变量 静态变量 常量
@@ -19,6 +34,11 @@ int main() {
   int a = 10;
   int b = 10;
   //打印 全局和局部变量地址：
-  printf("全局变量g_a 原始地址为 %p，十进制地址%d\n", &g_a,
-         (int)(size_t)&g_a); // 真难找，注意这里的表示方法
+  myPrint()(g_a);
+  myPrint()(g_b);
 }
+
+// printf("全局变量g_a 原始地址为 %p，十进制地址%d\n", &g_a,
+//        (int)(size_t)&g_a); // 真难找，注意这里的表示方法
+// printf("全局变量g_b 原始地址为 %p，十进制地址%d\n", &g_b, (int)(size_t)&g_b);
+// myPrint()(a);
