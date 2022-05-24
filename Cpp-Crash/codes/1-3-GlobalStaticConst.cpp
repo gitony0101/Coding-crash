f3d8edf22ad56b4b90f9c5b05c892e06df3aab79
gitevanlu <evanlu08@hotmail.com>
2022-05-24 19:56:52 +0800

    字符串常量
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- Cpp-Crash/codes/1-3-GlobalStaticConst.cpp
+++ Cpp-Crash/codes/1-3-GlobalStaticConst.cpp
@@ -29,6 +29,8 @@ int main() {
   /// 真难找，注意这里的表示方法:(int)(size_t)&g_a)
   printf("全局变量g_a 原始地址为 %p，十进制地址%d\n", &g_a, (int)(size_t)&g_a);
   printf("全局变量g_b 原始地址为 %p，十进制地址%d\n", &g_b, (int)(size_t)&g_b);
+  printf("未定义的全局变量g_c 原始地址为 %p，十进制地址%d\n", &g_c,
+         (int)(size_t)&g_c);
   printf("局部变量地址打印：\n");
   printf("局部变量a 原始地址为 %p，十进制地址%d\n", &a, (int)(size_t)&a);
   printf("局部变量b 原始地址为 %p，十进制地址%d\n", &b, (int)(size_t)&b);
@@ -58,6 +60,11 @@ int main() {
          (int)(size_t)&s_g_a);
   printf("静态全局变量s_g_b 原始地址为 %p，十进制地址%d\n", &s_g_b,
          (int)(size_t)&s_g_b);
+  printf("未定义的静态全局变量s_g_c 原始地址为 %p，十进制地址%d\n", &s_g_c,
+         (int)(size_t)&s_g_c);
+  //打印字符串常量
+  printf("打印字符串常量\n字符串常量hello world 原始地址为 %p，十进制地址%d\n",
+         &"hello world", (int)(size_t) & "hello world");
 }
 
 /*以下是之前的尝试：
