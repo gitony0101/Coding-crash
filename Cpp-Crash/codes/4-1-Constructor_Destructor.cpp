07340b52de4a6cde1bdaa8841e005f8e645ff55a
gitevanlu <cenclevan@yahoo.com>
2022-05-31 18:45:06 +0800

    构造函数和析构函数
    Signed-off-by: gitevanlu <cenclevan@yahoo.com>

---
+++ Cpp-Crash/codes/4-1-Constructor_Destructor.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+对象的初始化和清理
+1、构造函数 Constrctor 进行初始化操作
+没有返回值，不用void
+函数名称和类名一样
+可以有参数，能重载
+创建对象的时候，构造函数会自动调用一次，类比手机初始化一次好
+
+
+2、析构函数 Destructor 破坏机，进行清理的操作
+
+函数名和类名相同，但是要在名称前加`波浪号~`
+没有返回值不用void
+不可以有参数，不能重载（销毁用的，搞这些没意思）
+对象在销毁前，会且只会自动调用析构函数一次，类比重置手机一次就好
+
+构造和析构都是必须有的实现，如果没有，编译器会自己提供一个空实现
+*/
+
+class Person {
+public: // 一定要写出作用域
+  // 1、构造函数
+  Person() { printf("Person 构造函数调用\n"); }
+  // 2、析构函数
+  ~Person() { printf("Person 析构函数调用\n"); }
+};
+
+void test01() {
+  Person p1; // 在栈上的数据，test01执行完毕后，就会释放这个对象。
+}
+
+int main() {
+  test01();
+  Person p2; // 这里一般在任意键继续后执行，将来再讲，这里运行不出来
+  return 0;
+}
--- Cpp-Crash/codes/4-2-exClassYoutube.cpp
+++ Cpp-Crash/codes/4-2-exClassYoutube.cpp
@@ -22,12 +22,17 @@ void yt01() {
   yt01.OwnerName = "Tony";
   yt01.SubscribersCount = 10000;
   //   list<string> names{"A", "B", "C", "D", "E"}; 为什么这样会报错？
-  yt01.PublishedVideoTitles.push_back("Avengers: Endgame");
+  yt01.PublishedVideoTitles.push_back("Avengers: Endgame"); //添加节目
   yt01.PublishedVideoTitles.push_back("Avengers: Infinity War");
   yt01.PublishedVideoTitles.push_back("Avengers: Age of Ultron");
-  yt01.PublishedVideoTitles.push_back("Avengers: The Avengers");
   yt01.PublishedVideoTitles.push_back("Avengers: The Initiative");
   //打印yt01的信息
+  printf("Up主所有人姓名：%s\nUp主名称：%s\n订阅量：%d\n频道下节目:\n",
+         yt01.Name.c_str(), yt01.OwnerName.c_str(), yt01.SubscribersCount);
+  // 打印节目名称
+  for (auto title = yt01.PublishedVideoTitles.begin();
+       title != yt01.PublishedVideoTitles.end(); title++) // 循环起来啊，别忘了
+    printf("%s\n", title->c_str());
 }
 
-int main() {}
+int main() { yt01(); }
