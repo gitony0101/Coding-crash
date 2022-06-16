6f8e9ba8349b327cbc02c7f065dd62c806b4663d
gitevanlu <evanlu08@hotmail.com>
2022-06-16 11:34:21 +0800

    up
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- Cpp-Crash/codes/2-5-exArray.cpp
+++ Cpp-Crash/codes/2-5-exArray.cpp
@@ -2,12 +2,13 @@
 #include <cstdio>
 #include <iostream>
 #include <stdio.h>  // 动态数组用
-#include <stdlib.h> //动态数组用
+#include <stdlib.h> //动态数组用 内存分配  malloc calloc realloc free
 #include <string>
 
 using namespace std;
 /* 一维数组 二维数组
-一维数组 升序排列 逆转排列 冒泡排序
+一维数组
+  升序排列 逆转排列 冒泡排序
 1. `数据类型 数组名[ 数组长度 ];`
 2. `数据类型 数组名[ 数组长度 ] = { 值1，值2 ...};` # 把值也标出来
 3. `数据类型 数组名[ ] = { 值1，值2 ...};` # 长度可以自己推测出来，电脑会查
@@ -15,9 +16,16 @@ using namespace std;
 二维数组
 推荐使用：
 2. `数据类型 数组名[ 行数 ][ 列数 ] = { {数据1，数据2 } ，{数据3，数据4 } };`
-
-
 指针访问数组
+
+动态数组
+调整内存
+malloc 生成一个内存空间
+calloc 生成多个相同内存空间
+realloc 根据不同情况开辟不同大小的内存 （修改）有时返回的内存地址不一样
+free 释放某一个指针指向的内存空间，但是指针的指向不变
+一维动态数组int *array
+二维动态数组 int ** parray
 */
 
 //一维数组 从零开始，命名要规范，不要和变量重名
@@ -31,7 +39,7 @@ void exUnarry01() {
          sizeof(arr01), //对数组名进行 sizeof，可以获取整个数组占内存空间的大小
          sizeof(arr01[0])); //每个元素所占空间相同
   printf("数组首地址为%p，十进制地址为%d\n",
-         arr01, //直接打印数组名，可以查看数组所占内存的首地址
+         arr01, //直接打印数组名，可以查看数组所占内存的首地址7
          (int &)arr01); //注意这里的十进制转换
   printf("第一个元素地址为%p，十进制地址为%d\n", &arr01[0],
          (int &)arr01[0]); //注意首元素地址和数组首地址相同
@@ -164,8 +172,8 @@ void gradeStastics() {
 
 // 动态数组
 // 内存分配 malloc calloc
-void exDynamicArr01() {
-  int *parr = nullptr; // In newer C++ (C++11 and higher), use nullptr.
+void exDynamicArr01() { //一维动态数组
+  int *parr = nullptr;  // In newer C++ (C++11 and higher), use nullptr.
   int *parr01 = nullptr;
   // 开辟一个 int 4个字节的内存空间，用parr指针指向它
   parr = (int *)malloc(sizeof(int));
@@ -173,7 +181,8 @@ void exDynamicArr01() {
   *parr = 520; // 给parr赋值
   printf("赋值520后，parr的内存地址为：%p，parr的值为%d\n", &parr, *parr);
   // calloc 开辟动态数组
-  parr01 = (int *)calloc(5, 4); // 开辟5个内存空间，每个空间4字节的动态一维数组
+  parr01 =
+      (int *)calloc(5, 4); // 开辟5个内存空间，每个空间4字节的动态一维动态数组
   printf("初始化，parr01的内存地址为：%p，parr01的值为%d\n", &parr01, *parr01);
   //赋值parr01
   printf("赋值parr01并打印");
@@ -184,9 +193,13 @@ void exDynamicArr01() {
   cout << endl;
 }
 
-// calloc自定义动态数组
-void exDynamicArr02() {
-  //自定义动态数组parr01
+// calloc realloc free 自定义一维动态数组，free最后释放内存
+
+void exDynamicArr02() { //一维动态数组
+  //自定义动态数组 输入数组大小，calloc 按照大小开辟内存空间
+  //扳手 realloc 重新调整动态数组,根据不同情况开辟内存
+  // free 内存空间
+
   int num = 0;
   printf("请确定数组的大小：\n");
   scanf("%d", &num); // scanf输入，怎么需要&？
@@ -199,6 +212,45 @@ void exDynamicArr02() {
     printf("%d ", parr01[i]);
   }
   cout << endl;
+  printf("parr01 的地址为 %p\n", &parr01);
+  //扳手 realloc 上线，调整
+  int num01 = 0;
+  printf("请确定数组的大小：\n");
+  scanf("%d", &num01);
+  parr01 = (int *)realloc(parr01, num01);
+  printf("realloc 调整后，赋值parr01并打印\n");
+  for (int i = 0; i < num01; i++) {
+    parr01[i] = i + 1;
+    printf("%d ", parr01[i]);
+  }
+  cout << endl;
+  printf("调整后，parr01 的地址为 %p\n",
+         &parr01); // 有时返回的内存地址不一样，绝大多数情况是一样的，这是正常的
+
+  free(parr01); // free 释放内存空间，时刻注意释放不用的内存，基本素质
+  printf("free释放内存空间后，parr01 的地址为 %p，parr01数组现在是1：\n",
+         &parr01);
+  for (int i = 0; i < num01; i++) {
+    parr01[i] = i + 1;
+    printf("%d ", parr01[i]);
+  }
+  cout << endl;
+}
+
+// 二维动态数组
+void exDynamic2DArr() {
+  int **parr01; //创建二维动态数组
+  int m = 5, n = 6;
+  parr01 = (int **)calloc(
+      5, sizeof(int *)); //定义该数组
+                         // 看这里星星的层次：
+                         // int ** 表示的是parr01是二维的，几个星就几维
+  // calloc创建出来一条 含有5个指针的指针数组，指针类型是int一维指针所以是int*
+  //就是说，我们得到了5行。
+  for (int i = 0; i < 5;
+       i++) { //现在开始对每行进行操作，给每行各自开辟n个int内存空间
+    parr01[i] = (int *)calloc(n, sizeof(int)); //
+  } // 到这一步，未赋值的二维动态数组已经创建好了
 }
 
 int main() {
@@ -208,6 +260,7 @@ int main() {
   //   exBubbleSort01();
   // exBinArr01();
   // gradeStastics();
-  // exDynamicArr01();
-  exDynamicArr02();
+  // 内存分配调整 动态数组
+  // exDynamicArr01();// malloc calloc
+  // exDynamicArr02(); // 输入数组大小 calloc realloc
 }
