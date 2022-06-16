ef98e4156fe39af12666c418ebee79d210e4441f
gitevanlu <evanlu08@hotmail.com>
2022-06-16 21:06:49 +0800

    Update isinstance-type区别.py

--- Py-Crash/ch03/isinstance-type区别.py
+++ Py-Crash/ch03/isinstance-type区别.py
@@ -4,3 +4,6 @@ from collections import abc, OrderedDict, UserDict
 # isinstance():认为子类是一种父类类型，考虑继承关系
 # type():不会认为子类是一种父类类型，不考虑继承关系。
 # 如果要判断两个类型是否相同推荐使用isinstance()。
+
+my_dict = {}
+print(isinstance(my_dict, abc.Mapping), type(my_dict).__name__ == "dict")
