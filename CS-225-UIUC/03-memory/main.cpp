f5cd8645fc8cfb008bbc27ecdd309c397d074dbd
gitevanlu <evanlu08@hotmail.com>
2022-06-20 23:53:50 +0800

    update
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- CS-225-UIUC/03-memory/main.cpp
+++ CS-225-UIUC/03-memory/main.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
 #include "Cube.h"
+#include <iostream>
 
 int main() {
   cs225::Cube c;
   std::cout << "Address storing `c`:" << &c << std::endl;
 
   cs225::Cube *ptr = &c;
-  std::cout << "Addr. storing ptr: "<< &ptr << std::endl;
-  std::cout << "Contents of ptr: "<< ptr << std::endl;
+  std::cout << "Addr. storing ptr: " << &ptr << std::endl;
+  std::cout << "Contents of ptr: " << ptr << std::endl;
 
   return 0;
 }
