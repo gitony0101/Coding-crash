07ce9eeb0417a2be0fa7288aced067dd6ddcb507
gitevanlu <evanlu08@hotmail.com>
2022-06-26 06:26:28 +0800

    企业链表 图
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- CS-225-UIUC/DSA-2-1 企业链表/LinkList.h
+++ CS-225-UIUC/DSA-2-1 企业链表/LinkList.h
@@ -8,9 +8,11 @@ typedef struct LINKNODE {
 
 //链表结点
 typedef struct LINKLIST {
-  LinkNode head;
+  LinkNode head; //内存首地址
   int size;
 } LinkList;
+//打印企业链表结点指针
+typedef void (*PRINTNODE)(LinkNode *); // 与单向链表不同，这也是企业链表的特点
 
 //初始化链表
 LinkList *Init_LinkList();
@@ -19,7 +21,12 @@ LinkList *Init_LinkList();
 void Insert_LinkList(LinkList *list, int pos, LinkNode *data);
 //删除
 void Remove_LinkList(LinkList *list, int pos);
+//查找
+int Find_LinkList(LinkList *list, LinkNode *data);
 //返回链表大小
 int Size_LinkList(LinkList *list);
-
+//打印， print是main函数里面的打印函数MyPrint
+void Print_LinkList(LinkList *list, PRINTNODE print);
+//释放链表内存
+void FreeSpace_LinkList(LinkList *list);
 #endif
--- CS-225-UIUC/DSA-2-1 企业链表/main.cpp
+++ CS-225-UIUC/DSA-2-1 企业链表/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
+#include "LinkList.h"
 
-void myFunc() {}
+typedef struct MyData { //创建的是链表节点的结构体，每个都一样
+  LinkNode node;        //头部是node首地址，指向下一个node
+  char name[64];
+  int age;
 
-int main() {}
+} MyData;
+
+int main(void) {}
