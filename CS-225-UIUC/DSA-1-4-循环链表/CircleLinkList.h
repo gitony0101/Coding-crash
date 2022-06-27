ff28f793ff6e0a6b61556a05151e699406dc217e
gitevanlu <evanlu08@hotmail.com>
2022-06-27 08:00:55 +0800

    up

--- CS-225-UIUC/DSA-1-4-循环链表/CircleLinkList.h
+++ CS-225-UIUC/DSA-1-4-循环链表/CircleLinkList.h
@@ -10,13 +10,20 @@ typedef struct CIRCLELINKLIST {
   CircleLinkNode head; //头指针
   int size;            //链表大小
 } CircleLinkList;
-//
-typedef int(COMPARENODE)(CircleLinkNode *, CircleLinkNode *);
-// C没有bool返回值，这里返回int01，int(COMPARENODE)没有加星表示函数指针###注意下面的同步
-// 你对比的是什么？
-
 //自定义类型（结构体）创建完毕
+//创建需要的回调函数
+//比较回调函数
+typedef int(COMPARENODE)(CircleLinkNode *,
+                         CircleLinkNode *); //
+// 这里是C的写法，返回int01，C++可以bool，int(COMPARENODE)没有加星表示函数指针
+// ###注意下面的同步。你对比的是什么？
+//打印回调函数
+typedef void (*PRINTNODE)(CircleLinkNode *);
+//创建回调函数完毕
 
+//针对C定义的宏
+
+//
 //编写针对链表结构体操作的API函数
 //初始化循环链表
 CircleLinkList *Init_CircleLInkList();
@@ -35,7 +42,14 @@ void RemoveByValue_CircleLinkList(
     COMPARENODE *compare); // compare 这里加了星引用
 //获得链表的长度 int
 int SIze_CircleLinkList(CircleLinkList *clist);
+//判断
+int IsEmpty_CircleLinkList(CircleLinkList *clist);
 //查找 根据值去查找
 int Find_CircleLinkList(CircleLinkList *clist, CircleLinkNode *data,
-                        COMPARENODE *compare); // 有意思
+                        COMPARENODE *compare); // 有意思,还用了比较回调
+//打印结点
+void Print_CircleLinkList(CircleLinkList *clist, PRINTNODE print);
+//释放内存 释放我们自己申请的内存
+void FreeSpace_CircleLinkList(CircleLinkList *clist);
+
 #endif
--- CS-225-UIUC/DSA-1-4-循环链表/DSA-1-4-循环链表.md
+++ CS-225-UIUC/DSA-1-4-循环链表/DSA-1-4-循环链表.md
@@ -12,13 +12,17 @@
 
 
 
-### 调用函数
+### 回调函数
 
 ```cpp
 typedef int(COMPARENODE*)(CircleLinkNode *, CircleLinkNode *);
 ```
 
 根据值查找、删除都用到了回调函数，所以，你在compare什么？
+
+
+开发库的时候，有些情况我们考虑不到，数据类型是后来的人传进来的，所以我们要用这个回调函数
+
 
 
 
