23a27bafae7f1d3db7c992f32b7366ef7a16ff85
gitevanlu <evanlu08@hotmail.com>
2022-07-09 06:55:12 +0800

    update
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- CS-225-UIUC/DSA-1-3-5 中缀转后缀表达式 栈应用/main.cpp
+++ CS-225-UIUC/DSA-1-3-5 中缀转后缀表达式 栈应用/main.cpp
@@ -9,16 +9,32 @@ using namespace std;
 int IsNumber(char c) { //判断是否是数字， 也有库 isdigit
   return c >= '0' && c <= '9';
 }
-//直接输出数字
-void NumberOperate(const char *p) { printf("%c", *p); } // const char *p
+int IsLeft(char c) { //判断是不是左括号
+  return c == '(';
+}
+int IsRight(char c) { //判断是不是右括号
+  return c == ')';
+}
+int IsOperator(char c) { // 判断是不是运算符号
+  return c == '+' || c == '-' || c == '*' || c == '/';
+}
+//直接输出数字 const char *p
+void NumberOperate(const char *p) { printf("%c", *p); }
 /*
 c++类型更严格一些
 比如字符串字面量c是char*（但是你不能改，改了就爆炸，这算标准bug），c++就是char
 const*
 */
+//左括号的操作 直接进栈
+// 因为使用的企业链表形式的链式栈， 需要增加结构体
+typedef struct MYCHAR {
+  LinkNode node;
+  char *p;
+} MyChar;
+void LeftOperate(LinkStack *stack, char *p) { //对链表进行操作
+}
 
 int main() {
-
   const char *str = "8+(3-1)*5"; // 需要加const
   const char *p = str;           //还是需要讲究的
                                  //   //创建栈
@@ -28,6 +44,8 @@ int main() {
       NumberOperate(p);
     }
     p++; //既然要用while来循环，建议直接创建完while就来p++
+    if (IsLeft(*p)) { //如果是左括号 直接进栈
+    }
   }
   printf("\n");
   return 0;
