da45087de442579f96c25b373787be2431fbd81f
gitevanlu <evanlu08@hotmail.com>
2022-07-23 17:31:26 +0800

    随机初始化的数组有点问题，可以自定固定数组
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- CS-225-UIUC/DSA-3 排序/ex快速排序.cpp
+++ CS-225-UIUC/DSA-3 排序/ex快速排序.cpp
@@ -30,13 +30,50 @@ void InitMyArr(int arr[], int length) {
   }
 }
 
+//快速排序
+void QuickSort(int arr[], int left, int right) {
+  int i = left;       //定义开始下标
+  int j = right;      //定义末尾下标
+  int pivot = arr[i]; //取出第一个元素为基准数，并挖坑
+  if (i < j) {        //限定条件，保证左标一直小于右标
+    while (i < j) { //循环条件，每次只要左标还小于右标，那就往下循环。
+                    // 是为了递归设定的吗？
+      //从右向左开始找，找到第一个比基准数小的元素（准备放到左边去）
+      while (i < j && pivot < arr[j]) {
+        j--; //往前迭代下标
+      }
+      if (i < j) { //判断左标是否右边走i++
+
+        arr[i] = arr[j]; //把在右边找到的比基准数小的这个数移到左边
+        i++;             //左标往前走
+      }
+      //从左向右开始找，找到第一个比基准值大的元素（准备放到右边去）
+      while (i < j && pivot > arr[i]) {
+        i++; //往后迭代下标
+      }
+      if (i < j) {       //判断右标是否该往左边走j--
+        arr[j] = arr[i]; //把在左边找到的比基准数大的数移到右边
+      }
+    }
+    QuickSort(arr, left, i - 1);
+    QuickSort(arr, i + 1, right);
+  }
+}
+
 int main() {  //自定义创建随机数组并进行快速排序
   int length; // 定义数组长度
   printf("请输入一维数组大小：\n"); //单独写出来
   scanf("%d", &length);             //只负责输入
   int *Array = new int[length];
+  //随机初始化的数组有点问题，可以自定固定数组
+  //   int Array[] = {4, 2, 8, 0, 5, 7, 1, 3, 9};
+  //   int length = sizeof(Array) / sizeof(int);
   InitMyArr(Array, length);
   printf("未排序前的数组：\n");
+  PrintArray(Array, length);
+  printf("\n");
+  printf("快速排序后的数组：\n");
+  QuickSort(Array, 0, length - 1);
   PrintArray(Array, length);
   printf("\n");
   return 0;
