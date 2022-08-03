2f8b397a3d186386bc85fa66aa231a04deb85df0
gitevanlu <evanlu08@hotmail.com>
2022-08-03 22:38:45 +0800

    堆排序 02
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- CS-225-UIUC/DSA-3 排序/堆排序.cpp
+++ CS-225-UIUC/DSA-3 排序/堆排序.cpp
@@ -13,7 +13,7 @@ using namespace std;
 初始化堆 大顶堆 从小到大
 */
 //准备
-#define MAX 10
+#define MAX 15 设定序列的最大值
 //初始化一个随机数组
 void InitMyArr(int arr[], int length) {
   srand((unsigned)time(NULL));
@@ -26,21 +26,29 @@ void PrintArray(int arr[], int length) {
   for (int i = 0; i < length; i++) {
     printf("%d ", arr[i]);
   }
+  printf("\n");
 }
 //堆排序 函数：交换函数 调整堆  堆排序主函数
 // 交换函数
-void mySwap(int arr[], int a, int b) { // a b 为下标数
-  int temp = arr[a];                   //初始化temp存放一个值arr[a]
-  arr[a] = arr[b]; //把b处的值交换给a，此时两处值都是arr[b]
-  arr[b] = temp;   //把存放在temp的arr[a]的值换回来交给arr[b]
+// void mySwap(int arr[], int a, int b) { // a b 为下标数
+//   int temp = arr[a];                   //初始化temp存放一个值arr[a]
+//   arr[a] = arr[b]; //把b处的值交换给a，此时两处值都是arr[b]
+//   arr[b] = temp;   //把存放在temp的arr[a]的值换回来交给arr[b]
+// }
+void mySwap(int &a, int &b) {
+  int temp;
+  temp = a;
+  a = b;
+  b = temp;
 }
 // 调整堆
 /*
+heapify 二叉堆的顺序存储
   @param arr 待调整的数组
   @param index 待调整的结点下标
   @param length 数组的长度
 */
-void HeapAdjust(int arr[], int index, int length) {
+void Heapify(int arr[], int index, int length) {
   //先保存父结点下标,并假设：父结点就是最大的，大顶堆，从大到小，就这么安排
   int max = index;
   //保存左右孩子结点的数组下标：
@@ -56,7 +64,7 @@ void HeapAdjust(int arr[], int index, int length) {
     max = rChild;               //交换下标，现在rChild最大
   }
   if (max != index) { //交换两个结点
-    mySwap(arr, max, index);
+    mySwap(max, index);
     HeapAdjust(arr, max, length);
   }
 };
@@ -68,7 +76,8 @@ void HeapSort(int arr[], int length) {
   }
   //交换堆顶元素和最后一个元素
   for (int i = length - 1; i >= 0; i--) {
-    mySwap(arr, 0, i); //交换头尾
+    mySwap(0, i); //交换头尾
+    HeapAdjust(arr, i, 0);
   }
   printf("%d ", arr[0]);
   printf("\n");
