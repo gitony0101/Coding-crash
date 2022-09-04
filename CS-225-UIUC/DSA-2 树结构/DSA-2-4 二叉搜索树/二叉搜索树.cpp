b6876ab4aecb7a3b9660babd3acb1a5cfbaffcb4
gitevanlu <evanlu08@hotmail.com>
2022-09-04 10:21:31 +0800

    update
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- CS-225-UIUC/DSA-2 树结构/DSA-2-4 二叉搜索树/二叉搜索树.cpp
+++ CS-225-UIUC/DSA-2 树结构/DSA-2-4 二叉搜索树/二叉搜索树.cpp
@@ -15,7 +15,7 @@ struct BstNode {
 };                //分号别忘了，再次理解树结构的递归
 
 //在堆中初始化二叉搜索树（动态内存）
-BstNode *Init_BSTree(int data) {
+BstNode *GetNewNode(int data) { // GetNewNode函数名很形象
   // BstNode *newNode = (BstNode *)malloc(sizeof(BstNode));// mallc 写法
   BstNode *newNode = new BstNode();      // new 写法
   newNode->data = data;                  //初始化数据
@@ -24,12 +24,25 @@ BstNode *Init_BSTree(int data) {
 }
 
 //创造在结点中插入数值的函数insertData
-BstNode *insertData(BstNode *root, int data) { return root; }
+BstNode *insertData(BstNode *root, int data) {
+  if (root == NULL) {        // 空树
+    root = GetNewNode(data); //为什么
+  }
+  //下面如果不是空树，我们有两种情况需要考虑，小的放到左子树，大的放到右子树
+  else if (data <= root->data) { //父节点数值比插入数值大，那就放到左子树
+    root->left = insertData(root->left, data);
+  }
+  //剩下的就是父节点数值比插入结点数值小，按照二叉搜素树要求，放到右结点
+  else {
+    root->right = insertData(root->right, data);
+  }
+  return root;
+}
 
 int main() {
   BstNode *root = NULL; //创建一个空树
-  insertData(root, 15);
-  insertData(root, 20);
+  root = insertData(root, 15);
+  root = insertData(root, 20);
   printf("\n");
   return 0;
 }
