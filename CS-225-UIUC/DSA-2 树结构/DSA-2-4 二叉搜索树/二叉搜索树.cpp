c220a4a2dd0daff096778b92892230f1794bd10c
gitevanlu <evanlu08@hotmail.com>
2022-09-04 10:50:18 +0800

    二叉搜索树
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- CS-225-UIUC/DSA-2 树结构/DSA-2-4 二叉搜索树/二叉搜索树.cpp
+++ CS-225-UIUC/DSA-2 树结构/DSA-2-4 二叉搜索树/二叉搜索树.cpp
@@ -23,7 +23,7 @@ BstNode *GetNewNode(int data) { // GetNewNode函数名很形象
   return newNode;
 }
 
-//创造在结点中插入数值的函数insertData
+//创造在结点中插入数值的函数insertData，从而可以创造二叉搜索树
 BstNode *insertData(BstNode *root, int data) {
   if (root == NULL) {        // 空树
     root = GetNewNode(data); //为什么
@@ -38,11 +38,35 @@ BstNode *insertData(BstNode *root, int data) {
   }
   return root;
 }
+// 二叉搜索树搜索bool函数 还是用到了递归Search
+bool Search(BstNode *root, int data) {
+  if (root == NULL)
+    return false;
+  else if (root->data == data)
+    return true;               //特例防呆
+  else if (root->data >= data) //如果插入值小于父节点值，往左递归
+    return Search(root->left, data);
+  else //如果插入值大于父节点值，往右递归
+    return Search(root->right, data);
+  //总之牢记二叉搜索树两个子结点与父节点的关系就是：左小右大（与父节点比）
+}
 
 int main() {
   BstNode *root = NULL; //创建一个空树
+  //按照insertData插入结点，从而创作二叉搜索树
   root = insertData(root, 15);
+  root = insertData(root, 10);
   root = insertData(root, 20);
+  root = insertData(root, 25);
+  root = insertData(root, 8);
+  root = insertData(root, 12);
+  int num;
+  printf("请输入数值:\n");
+  scanf("%d", &num);
+  if (Search(root, num) == true)
+    printf("在二叉搜索树中找到了这个数");
+  else
+    printf("没有这个数。");
   printf("\n");
   return 0;
 }
