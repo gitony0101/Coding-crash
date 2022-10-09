8b33bad57fe30150c478683a79316f33e1174e4b
gitevanlu <evanlu08@hotmail.com>
2022-10-10 00:02:02 +0800

    template
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- Cpp-Crash/source/5-1-template.cpp
+++ Cpp-Crash/source/5-1-template.cpp
@@ -4,6 +4,34 @@
 #include <stdlib.h>
 #include <string.h>
 using namespace std;
+
+//引入模板：类型参数化，编写代码可以忽略类型，使用一个符号代替所有符号
+//为了让编译器区分是普通 模板函数
+template <class T> // template<typename T> 关键字不同但是意思相同
+void MySwap(T &a, T &b) {
+  T temp = a;
+  a = b;
+  b = temp;
+}
+
+int main() {
+  //函数模板可以自动类型推导
+  int a = 10, b = 20;
+  double c = 1.1, d = 4.1;
+  printf("a: %d b : %d\n", a, b);
+  MySwap(a, b);
+  printf("Now a: %d b : %d\n", a, b);
+  printf("c: %f d : %f\n", c, d);
+  MySwap(c, d);
+  printf("Now c: %f d : %f\n", c, d);
+  // 显式制定类型
+  MySwap<int>(a, b);
+  printf("Again a: %d b : %d\n", a, b);
+
+  printf("\n");
+  return 0;
+}
+
 // // int 类型数据交换
 // void MySwap(int &a, int &b) {
 //   int temp = a;
@@ -17,20 +45,6 @@ using namespace std;
 //   b = temp;
 // }
 //以上是重复操作，容易出错，维护成本高
-//引入模板：类型参数化，编写代码可以忽略类型，使用一个符号代替所有符号
-//为了让编译器区分是普通 模板函数
-template <class T> // template<typename T> 关键字不同但是意思相同
-void MySwap(T &a, T &b) {
-  T temp = a;
-  a = b;
-  b = temp;
-}
-
-int main() {
-
-  printf("\n");
-  return 0;
-}
 
 /*
 
