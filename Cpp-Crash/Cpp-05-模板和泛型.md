e822e6410411d021d5b99b4c5595e6533b10dc36
gitevanlu <evanlu08@hotmail.com>
2022-10-14 09:01:10 +0800

    compilation process
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- Cpp-Crash/Cpp-05-模板和泛型.md
+++ Cpp-Crash/Cpp-05-模板和泛型.md
@@ -11,6 +11,7 @@ Template  and Generic
 模板的特点：
 
 - 模板不可以直接使用，它只是一个框架
+- 用什么类型-函数模板自动匹配函数的类型，自动变成相应类型
 - 模板的通用并不是万能的
 
 C++提供两种模板机制:
@@ -21,15 +22,20 @@ C++提供两种模板机制:
 
 > 函数模板作用：建立一个通用函数，其函数返回值类型和形参类型可以不具体制定，用一个**虚拟的类型**来代表。
 
+比如：
+
+- 顺序队列 内部是数组--类模板
+- 链式队列 内部是链表--类模板
+
 **语法：**
 
 ```C++
 template<typename T>
-// 函数声明或定义
-//或者
+// 函数声明或定义 或者
 template<class T> //关键字不同但是意思相同
-
 ```
+
+> 习惯建议用`typename`避免与`class`混淆
 
 **解释：**
 
@@ -37,11 +43,11 @@ template --- 声明创建模板
 
 typename --- 表面其后面的符号是一种数据类型，可以用 class 代替
 
-T --- 通用的数据类型，名称可以替换，通常为大写字母
+T --- 通用的数据类型，名称可以替换，通常为大写字母，它就是个行参
 
-### `template<typename/class T>`只对它后面第一个函数生效
+- `template<typename/class T>`只对它后面第一个函数生效
 
-### `template<class T1,class T2,class T3>`可以这样命名下去
+- `template<class T1,class T2,class T3>`可以这样命名下去
 
 总结：
 
@@ -57,143 +63,28 @@ T --- 通用的数据类型，名称可以替换，通常为大写字母
 > 
 - 使用模板时必须确定出通用数据类型 T，并且能够推导出一致的类型
   - 遇上循环了怎么办，  `printf`用`u`：
-```cpp
+```c++
 template <typename T>
 void printArray(T arr[], int len) {
   for (int i = 0; i < len; i++) {
     printf("%u", arr[i]);//直接用的T来指代
   }
-  ```
-
-
-
-
-
-
-
-示例：
-
-```C++
-//交换的函数模板
-template<typename T>
-void mySwap(T &a, T&b)
-{
-	T temp = a;
-	a = b;
-	b = temp;
-}
-
-
-template<class T> // 也可以替换成typename
-//利用选择排序，进行对数组从大到小的排序
-void mySort(T arr[], int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		int max = i; //最大数的下标
-		for (int j = i + 1; j < len; j++)
-		{
-			if (arr[max] < arr[j])
-			{
-				max = j;
-			}
-		}
-		if (max != i) //如果最大数的下标不是i，交换两者
-		{
-			mySwap(arr[max], arr[i]);
-		}
-	}
-}
-template<typename T>
-void printArray(T arr[], int len) {
-
-	for (int i = 0; i < len; i++) {
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-}
-void test01()
-{
-	//测试char数组
-	char charArr[] = "bdcfeagh";
-	int num = sizeof(charArr) / sizeof(char);
-	mySort(charArr, num);
-	printArray(charArr, num);
-}
-
-void test02()
-{
-	//测试int数组
-	int intArr[] = { 7, 5, 8, 1, 3, 9, 2, 4, 6 };
-	int num = sizeof(intArr) / sizeof(int);
-	mySort(intArr, num);
-	printArray(intArr, num);
-}
-
-int main() {
-
-	test01();
-	test02();
-
-	system("pause");
-
-	return 0;
-}
 ```
 
-总结：模板可以提高代码复用，需要熟练掌握
 
-## 6. 普通函数与函数模板的区别
 
-**普通函数与函数模板区别：**
 
-- 普通函数调用时可以发生自动类型转换（隐式类型转换）
-- 函数模板调用时，如果利用自动类型推导，不会发生隐式类型转换
-- 如果利用显示指定类型的方式，可以发生隐式类型转换
 
-**示例：**
 
-```C++
-//普通函数
-int myAdd01(int a, int b)
-{
-	return a + b;
-}
 
-//函数模板
-template<class T>
-T myAdd02(T a, T b)
-{
-	return a + b;
-}
+### 函数模板可以像普通函数那样被重载
 
-//使用函数模板时，如果用自动类型推导，不会发生自动类型转换,即隐式类型转换
-void test01()
-{
-	int a = 10;
-	int b = 20;
-	char c = 'c';
+### 函数模板与普通函数的区别
 
-	cout << myAdd01(a, c) << endl; //正确，将char类型的'c'隐式转换为int类型  'c' 对应 ASCII码 99
+- 是否允许**自动类型转**化：普通函数**不允许**，行参定义了啥就是啥，函数模板**允许**，灵活
+> #### 本质是模板可以根据实际的使用场景自动编译
 
-	//myAdd02(a, c); // 报错，使用自动类型推导时，不会发生隐式类型转换
-
-	myAdd02<int>(a, c); //正确，如果用显示指定类型，可以发生隐式类型转换
-}
-
-int main() {
-
-	test01();
-
-	system("pause");
-
-	return 0;
-}
-```
-
-总结：建议使用显示指定类型的方式，调用函数模板，因为可以自己确定通用类型 T
-
-## 7. 普通函数与函数模板的调用规则
+### 普通函数与函数模板的调用规则
 
 调用规则如下：
 
--- Cpp-Crash/source/5-1-template.cpp
+++ Cpp-Crash/source/5-1-template.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 //引入模板：类型参数化，编写代码可以忽略类型，使用一个符号代替所有符号
-//为了让编译器区分是普通 模板函数
+
+// 1、为了让编译器区分是普通 模板函数
 template <class T> // template<typename T> 关键字不同但是意思相同
 //每次用一次都要调用一次
 void MySwap(T &a, T &b) {
@@ -15,8 +16,47 @@ void MySwap(T &a, T &b) {
   b = temp;
 }
 
+/*
+2、 函数模板案例
+- 利用函数模板封装一个排序的函数，可以对**不同数据类型数组**进行排序
+- 排序规则从大到小，排序算法为**选择排序**
+- 分别利用**char 数组**和**int 数组**进行测试
+*/
+
+//打印函数模板
+template <typename T> void printArray(T arr[], int len) {
+  for (int i = 0; i < len; i++) {
+    printf("%u", arr[i]);
+  }
+  printf("\n");
+}
+template <typename T>
+// template <class T> 这俩都可以用
+void mySort(T arr[], int len) { //选择排序
+  for (int i = 0; i < len; i++) {
+    int max = i;
+    for (int j = i + 1; j < len; j++) {
+      if (arr[max] < arr[j]) {
+        max = j; //交换下标
+      }
+    }
+    if (max != i) {
+      MySwap(arr[max], arr[i]);
+    }
+  }
+}
+
+// 3、
+// 函数模板和普通函数的区别：是否允许**自动类型转**化：函数模板**不允许**，普通函数**允许**
+template <class T> int MyAdd(T a, T b) { return a + b; }
+int MyAdd(int a, char c) { return a + c; }
+
+// 4、函数模板可以想像普通函数那样被重载
+template <typename T> void myPrint(T a) { printf("%u", a); }
+template <typename T> void myPrint(T a, T b) { printf("%u %u", a, b); }
+
 int main() {
-  //函数模板可以自动类型推导
+  // 1、函数模板可以自动类型推导
   int a = 10, b = 20;
   double c = 1.1, d = 4.1;
   printf("a: %d b : %d\n", a, b);
@@ -28,129 +68,23 @@ int main() {
   // 显式制定类型
   MySwap<int>(a, b);
   printf("Again a: %d b : %d\n", a, b);
-
   printf("\n");
+  // 2 函数模板案例 排序
+  char charArr[] = "asdfasdfgg";
+  int num_1 = sizeof(charArr) / sizeof(char);
+  int intArr[] = {7, 5, 8, 1, 3, 9, 2, 4, 6};
+  int num_2 = sizeof(intArr) / sizeof(int);
+  mySort(charArr, num_1);
+  mySort(intArr, num_2);
+  printArray(charArr, num_1);
+  printArray(intArr, num_2);
+  printf("\n");
+  // 3、函数模板和普通函数的区别：是否允许**自动类型转**化：函数模板**不允许**，普通函数**允许**
+  char c1 = 'a', c2 = 'b';
+  int test1 = MyAdd(a, c1); // 会调用普通函数
+  int test2 = MyAdd(b, a); //会调用函数模板，两个int不符合普通函数的行参要求
+  //要是想强制调用模板：
+  int test3 = MyAdd<>(a, b); //一般不这么做
+
   return 0;
 }
-
-/*
-函数模板案例
-- 利用函数模板封装一个排序的函数，可以对**不同数据类型数组**进行排序
-- 排序规则从大到小，排序算法为**选择排序**
-- 分别利用**char 数组**和**int 数组**进行测试
-*/
-
-// template <typename T>
-
-// void MySwap(T &a, T &b) {
-//   T temp = a;
-//   a = b;
-//   b = temp;
-// }
-
-// template <typename T>
-// // template <class T> 这俩都可以用
-// void mySort(T arr[], int len) {
-//   for (int i = 0; i < len; i++) {
-//     int max = i;
-//     for (int j = i + 1; j < len; j++) {
-//       if (arr[max] < arr[j]) {
-//         max = j;
-//       }
-//     }
-//     if (max != i) {
-//       MySwap(arr[max], arr[i]);
-//     }
-//   }
-// }
-
-// template <typename T> void printArray(T arr[], int len) {
-//   for (int i = 0; i < len; i++) {
-//     printf("%u", arr[i]);
-//   }
-//   printf("\n");
-// }
-
-// int main() {
-//   char charArr[] = "asdfasdfgg";
-//   int num_1 = sizeof(charArr) / sizeof(char);
-//   int intArr[] = {7, 5, 8, 1, 3, 9, 2, 4, 6};
-//   int num_2 = sizeof(intArr) / sizeof(int);
-//   mySort(charArr, num_1);
-//   mySort(intArr, num_2);
-//   printArray(charArr, num_1);
-//   printArray(intArr, num_2);
-
-//   printf("\n");
-//   return 0;
-// }
-
-// // int 类型数据交换
-// void MySwap(int &a, int &b) {
-//   int temp = a;
-//   a = b;
-//   b = temp;
-// }
-// // double 类型转换
-// void MySwap(double &a, double &b) {
-//   int temp = a;
-//   a = b;
-//   b = temp;
-// }
-//以上是重复操作，容易出错，维护成本高
-
-/*
-
-//交换整型函数
-void swapInt(int& a, int& b) {
-        int temp = a;
-        a = b;
-        b = temp;
-}
-
-//交换浮点型函数
-void swapDouble(double& a, double& b) {
-        double temp = a;
-        a = b;
-        b = temp;
-}
-
-//利用模板提供通用的交换函数
-template<typename T>
-void mySwap(T& a, T& b)
-{
-        T temp = a;
-        a = b;
-        b = temp;
-}
-
-void test01()
-{
-        int a = 10;
-        int b = 20;
-
-        //swapInt(a, b);
-
-        //利用模板实现交换
-        //1、自动类型推导
-        mySwap(a, b);
-
-        //2、显示指定类型
-        mySwap<int>(a, b);
-
-        cout << "a = " << a << endl;
-        cout << "b = " << b << endl;
-
-}
-
-int main() {
-
-        test01();
-
-        system("pause");
-
-        return 0;
-}
-
-
-*/
