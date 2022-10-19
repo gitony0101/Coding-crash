bca82056c3dbe2686370bed6dd8a4ce6300852e1
gitevanlu <evanlu08@hotmail.com>
2022-10-19 09:48:33 +0800

    up
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- Cpp-Crash/source/4-1-virtual-function.cpp
+++ Cpp-Crash/source/4-1-virtual-function.cpp
@@ -14,10 +14,10 @@ using namespace std;
     抽象类的特性：不能创建对象，但是可以创建对象指针
     ADT 我猜就是Abstract Data Type 抽象数据类型
 3、虚析构函数（析构函数可以（往往）是虚函数）
- 是因为父类指针被子类对象初始化的析构会出现问题
+    父类指针，被子类初始化，会出现内存泄漏问题
 */
 
-// 1、虚函数对内存影响
+//// 1、虚函数对内存影响
 class emptyClass00 {
   //对于一个空的类，占用1个内存字节；用来内存标记，告诉程序这个类是一个自定义类型
 };
@@ -36,53 +36,99 @@ public:
   virtual void print04() { printf("虚函数 4\n"); }
 };
 
-// 2、 纯虚函数 有种写有文件h的感觉
+//// 2、 纯虚函数 有种写有文件h的感觉
+// stack  栈
 class stack {
 public:
-  //纯虚函数:
-  virtual void push(int element) = 0; //重点在写法：没有函数体
-  virtual void pop();                 //不写=0好像也行
+  //抽象一个父类,已经知道栈的基本操作,把所有函数声明为抽象数据类型,父类中所有的属性描述好
+  virtual void push(int data) = 0; //重点在写法：没有函数体
+  virtual void pop() = 0;
   virtual int top() const = 0;
-  virtual int size() = 0;
-  virtual bool empty() const = 0; // const也能写
+  virtual bool empty() const = 0;
+  virtual int size() const = 0;
 };
-// ADT（抽象数据类型）写两个子类：数组栈、链式栈
-//过程：子类想要创建对象，必须要重写纯虚函数中的过程
-struct Node { //实现数组栈 结点结构体
+// // ADT（抽象数据类型）写两个子类：数组栈、链式栈
+// //过程：子类想要创建对象，必须要重写纯虚函数中的过程
+class arrayStack : public stack //数组栈---用数组实现一个栈
+{
+public:
+  void push(int data) //入栈---入一个整数
+  {}
+  void pop() //出栈
+  {}
+  int top() const //获取栈最上面的元素
+  {
+    return 1;
+  }
+  bool empty() const //判断是否为空
+  {
+    return true;
+  }
+  int size() const //获取大小
+  {
+    return 1;
+  }
+  //可以增加别的函数---但是纯虚函数必须要重写
+  //可以增加别的数据成员
+protected:
+  int *array; //数组栈---准备数组的指针
+};
+struct Node //节点---用链表实现,需要结构体
+{
   int data;
-  Node *next; //下面链接
+  Node *next;
 };
-class arrayStack : public stack { //创建数组栈子类，
-public:                           // 开辟括号，有种写h的感觉
-  void push(int element){};       //入栈，入一个整数，全都是模拟
-  void pop();                     //出栈
-  int top() { return 1; };        //获取站定元素
-  int size();                     //获取大小
-  bool empty() { return true; };  //             //判断是否为空
-protected:
-  int *array; //数组栈 准备数组的指针
-};
-class listStack : public stack { //创建链栈子类
-public:                          // 开辟括号，有种写h的感觉
-  void push(int element){};      //入栈，入一个整数，全都是模拟
-  void pop();                    //出栈
-  int top() { return 1; };       //获取站定元素
-  int size();                    //获取大小
-  bool empty() { return true; }; //             //判断是否为空
-protected:
-  Node *headnode; //表头
-};
+//不管做什么栈,重写抽象类即可
+class listStack : public stack //链式栈---用链表实现一个栈
+{
+public:
+  void push(int data) {}
+  void pop() {}
+  int top() const { return 1; }
+  bool empty() const { return true; }
+  int size() const { return 1; }
 
+protected:
+  Node *headNode; //表头
+};
 // 测试栈函数
-void testStack(stack *pStack) { //测试代码：传入抽象类的指针
-  pStack->push(1);              //
-  while (!pStack->empty()) {
-    printf("%d\n", pStack->top());
-    pStack->pop(); // 弹出栈顶元素
+void testStack(stack *pStack) //测试代码---传入抽象类的指针
+{
+  pStack->push(1);         //入栈,入1
+  while (!pStack->empty()) //判断栈是否为空
+  {
+    cout << pStack->top(); //如果栈不为空,获取栈顶元素,不断出栈
+    pStack->pop();
   }
 }
 
-// 3、虚析构函数
+//// 3、虚析构函数（析构函数可以（往往）是虚函数）用法
+//父类指针，被子类初始化，会出现内存泄漏问题
+class A {
+public:
+  ~A() { // 父类析构
+    printf("父类析构函数\n");
+  }
+};
+class B : public A { //创建A的子类B
+public:
+  ~B() { //同样创建子类析构
+    printf("子类析构函数\n");
+  }
+};
+//解决方案：将父类的析构函数，加上virtual成为：
+//“虚析构函数”
+class C {
+public:
+  virtual ~C() {
+    //创建父类虚析构函数
+    printf("父类C，虚析构函数\n");
+  };
+};
+class D : public C {
+public:
+  ~D() { printf("D子类析构函数\n"); }
+};
 
 int main() {
   // 1、虚函数对内存影响
@@ -95,12 +141,21 @@ int main() {
   // 2、 纯虚函数
   {
     // stack myStack; //抽象类不能创建对象，看一下报错
-    arrayStack *pArrStack = new arrayStack;
-    testStack(pArrStack);
 
+    //无论什么方式实现,都可以调用这个测试代码
+    //用数组|链表调用,测试代码是一样的,统一接口
+    testStack(new arrayStack);
     testStack(new listStack);
   }
   printf("\n");
+  // 3、虚析构函数（析构函数可以（往往）是虚函数）
+  {
+    A *pA = new B; //父类指针，被子类初始化，会出现内存泄漏问题
+    delete pA;     //删除pA后注意看出来的事哪个析构函数
+                   //解决方案：创建父类虚析构函数
+    C *pC = new D;
+    delete pC; // 注意调用顺序，洋葱结构
+  }
   printf("\n");
   printf("\n");
   printf("\n");
@@ -123,5 +178,6 @@ D 里面有两个A，会产生二义性问题：
 
 1.因为当你顶级基类A有一虚函数play();，而A的子类B也有play();，A的子类C也有play();，这就说明如果顶级基类A中的play();若定义怎么玩是不合理的。因为B有B的玩法C有C的玩法。所以这种情况下定义为纯虚函数目的在于使子类仅仅只是继承函数的接口，告诉子类的设计者“你必须在子类中提供一个纯虚函数的实现，但我不知道你会怎么样实现它”
 2.引入纯虚函数，则编译器要求在子类中必须予以重写以实现多态性
+https://blog.csdn.net/weixin_60569662/article/details/121757187
 
 */
