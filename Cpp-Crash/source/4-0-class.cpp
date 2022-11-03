246d885227e559ac38f90753239f32bcc8f2c246
gitevanlu <evanlu08@hotmail.com>
2022-11-03 11:19:04 +0800

    class
    Signed-off-by: gitevanlu <evanlu08@hotmail.com>

--- Cpp-Crash/source/4-0-class.cpp
+++ Cpp-Crash/source/4-0-class.cpp
@@ -8,6 +8,8 @@ using namespace std;
 const double pi = 3.14;
 /*
 class 类方法
+类中的变量称为成员变量、成员属性
+类中的函数称为成员函数、成员方法
 
 */
 //设计一个类，求圆的周长
@@ -16,17 +18,22 @@ class Circle {
 public:                                              //公共作用域
   double getCircumference() { return 2 * pi * m_R; } // 求周长公式
   double m_R;
-  //设定圆半径
-  void setR(int r) { m_R = r; }
-  //获取圆当前半径
-  int getR() { return m_R; }
+
+  void setR(int r) { //设定圆半径
+    m_R = r;
+  }
+  int getR() { //获取圆当前半径,注意函数类型
+    return m_R;
+  }
 };
 
 int main() {
   Circle c1; // 实例化对象：通过类来创建对象
   c1.setR(10);
+  printf("圆的半径为%d\n", c1.getR());
   double cir1 = c1.getCircumference();
-  printf("圆的周长为： %f", cir1);
+
+  printf("圆的周长为：%f\n", cir1);
 
   printf("\n");
   return 0;
