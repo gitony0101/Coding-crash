76ab379d552d26c643f53ca40fac2526a82812ba
gitevanlu <53283254+gitony0101@users.noreply.github.com>
2022-12-09 18:39:51 +0800

    update
    Signed-off-by: gitevanlu <53283254+gitony0101@users.noreply.github.com>

--- Cpp-Crash/Cpp-02-操作符和语句.md
+++ Cpp-Crash/Cpp-02-操作符和语句.md
@@ -83,6 +83,29 @@ i++ 与 ++i 的主要区别有两个：
 
 codes: ex,BMI,Integer,Lunar year
 
+```cpp
+#include <stdio.h>
+using namespace std;
+
+int main() { // if else
+  int price = 0;
+  int bill = 0;
+  printf("请输入金额:\n");
+  scanf("%d", &price);
+  printf("请输入票面\n");
+  scanf("%d", &bill);
+  if (bill >= price) {
+    printf("应该找您:%d \n", bill - price);
+  } else {
+    printf("您的钱不够，还差%d", price - bill);
+  }
+
+  printf("\n");
+  return 0;
+}
+
+```
+
 ## 判断语句
 
 - If-else 语句
--- Cpp-Crash/source/test/test.cpp
+++ Cpp-Crash/source/test/test.cpp
@@ -1,20 +1,20 @@
 // #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 using namespace std;
 
-void myFunc() {}
-
-int main() {
-  int a = 5;
-  int b = 6;
-  int temp;
-  temp = a;
-  a = b;
-  b = temp;
-  printf("a = %d,b = %d\n", a, b);
+int main() { // if else
+  int price = 0;
+  int bill = 0;
+  printf("请输入金额:\n");
+  scanf("%d", &price);
+  printf("请输入票面\n");
+  scanf("%d", &bill);
+  if (bill >= price) {
+    printf("应该找您:%d \n", bill - price);
+  } else {
+    printf("您的钱不够，还差%d", price - bill);
+  }
 
   printf("\n");
   return 0;
