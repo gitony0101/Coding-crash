74542f77874b90df6f55b00d740f58c3973f6848
gitevanlu <53283254+gitony0101@users.noreply.github.com>
2022-12-20 09:25:48 +0800

    hash table set map
    Signed-off-by: gitevanlu <53283254+gitony0101@users.noreply.github.com>

---
+++ CS225-UIUC/DSA-4 哈希表/hash-function.cpp
@@ -0,0 +1,24 @@
+// #define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
+
+#define CAPACITY 50000 // Size of the Hash Table
+
+unsigned long hash_function(char *str) {
+  unsigned long i = 0;
+  for (int j = 0; str[j]; j++)
+    i += str[j];
+  return i % CAPACITY;
+}
+
+int main() {
+  string str01 = "GLE";
+
+  hash_function(str01);
+
+  printf("\n");
+  return 0;
+}
---
+++ CS225-UIUC/DSA-4 哈希表/map-unordered-map.cpp
@@ -0,0 +1,16 @@
+// #define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+#include <map>
+#include <unordered_map>
+
+void myFunc() {}
+
+int main() {
+
+  printf("\n");
+  return 0;
+}
