f6889636dbbbe0f536561f28ad933d9123016feb
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2023-03-31 21:40:33 +0800

    update
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- PyGuiQt/source/tempCodeRunnerFile.py
+++ PyGuiQt/source/tempCodeRunnerFile.py
@@ -1,14 +1,1 @@
-     # label = QLabel("Py GUI Dev", self)
-        # # label.setText(str(12)) #那么这里设定的就是默认值
-        # label.setNum(15)
-        # label.setText("Fly Emirates")
-        # label.move(220, 150)  # 坐标式移动
-        # label.setFont(QFont("Emirates", 42))  # QFont包
-        # label.setStyleSheet('color:red')
-
-        # line_edit = QLineEdit(self)
-        # line_edit.setFont(QFont("Emirates", 22))
-        # # line_edit.setText("Fly Better!")
-        # line_edit.setPlaceholderText("Please enter your destination here...")
-        # line_edit.setGeometry(0, 0, 300, 50)
-        # line_edit.setEnabled(False)  # 虚化边框
+i
