9abf8849f453d2e4bdda8851b0cc3231004fe856
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2023-05-15 06:31:08 +0800

    update
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- Cpp-Crash/examples/test.cpp
+++ Cpp-Crash/examples/test.cpp
@@ -4,27 +4,12 @@
 #include <random>
 #include <stdio.h>
 using namespace std;
+#include <iostream>
 
 int main() { // if else
-  srand(time(0));
-  int rMin, rMax = 0;
-  int input_number = 0;
-  printf("请输入想猜的数字范围:\n");
-  scanf("%d %d", &rMin, &rMax);
-  int randNum = (rand() % (rMax - rMin + 1)) + rMin;
-  printf("请输入你的数字:\n");
-  scanf(" %d", &input_number);
-  while (input_number != randNum) {
-    if (input_number < randNum) {
-      printf("小了\n");
-    } else {
-      printf("大了\n");
-    }
-    printf("请重新输入你的数字\n");
-    scanf("%d", &input_number);
+  if (4 & 2) {
+    printf("True.");
+  } else {
+    printf("False.");
   }
-
-  printf("\n恭喜你答对了,这个数字就是  %d\n", randNum);
-  printf("\n");
-  return 0;
 }
